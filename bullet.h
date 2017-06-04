ca3df28567ebbd99f120fbb20c4c8a23f0ec348d
SudSwastik <sudarshan4309@gmail.com>
2017-06-04 21:06:32 +0530

    added bullet

---
+++ bullet.h
@@ -0,0 +1,51 @@
+class bullet
+{
+	public:
+		double x;
+		double y;
+		int firing;
+		bullet()
+		{
+			firing=0;
+		}
+		void getPosition(myship ship)
+		{
+			x=ship.x;
+			y=ship.y+35;
+		}
+		void fire()
+		{
+
+			firing=1;
+		}
+		void draw()
+		{
+			const float DEG2RAD = 3.14159/180;
+
+			glColor3f(1,0,0);
+			glLineWidth(3);
+//			glBegin(GL_LINES);
+//				glVertex2f(x,y);
+//				glVertex2f(x,y+10);
+//			glEnd();
+            glBegin(GL_POLYGON);
+
+            for (int i=0; i<360; i++)
+            {
+                float degInRad = i*DEG2RAD;
+                glVertex2f(x+5+cos(degInRad)*3,y+5+sin(degInRad)*3);
+                //glVertex2f(x+cos(degInRad)*5,y+sin(degInRad)*5+10);
+            }
+
+   glEnd();
+
+		}
+		void move(int offset)
+		{
+			y=y+offset;
+		}
+		void reinit()
+		{
+			firing=0;
+		}
+};
